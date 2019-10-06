1b28af70ae733d9009d93822c9c4edbe90161524
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2019-10-06 18:39:51 -0400

    Temporarily switched androidmanifest back to testactivity, created getRoutes function, created Routes class

--- app/src/main/java/com/pantone448c/ltccompanion/GTFSStaticData.java
+++ app/src/main/java/com/pantone448c/ltccompanion/GTFSStaticData.java
@@ -1,4 +1,57 @@
 package com.pantone448c.ltccompanion;
 
+import android.util.Log;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.TreeMap;
+
 public class GTFSStaticData {
+    public static TreeMap<Integer, Route> getRoutes(InputStream in)
+    {
+        TreeMap<Integer, Route> routes = new TreeMap<>();
+
+        Reader read = new InputStreamReader(in);
+
+        try
+        {
+            Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(read);
+            int count = 0;
+            for (CSVRecord record : records)
+            {
+                if (count > 0)//skip headers
+                {
+                    int route_id = Integer.parseInt(record.get(0)); //will always have a value as it's required by GTFS standard
+                    int route_name = Integer.parseInt(record.get(2));
+                    int route_colour = 0;
+                    int route_text_colour = 0;
+                    if (!record.get(7).isEmpty())
+                    {
+                        route_colour = Integer.parseInt(record.get(7), 16);
+                    }
+                    if (!record.get(8).isEmpty())
+                    {
+                        route_text_colour = Integer.parseInt(record.get(8), 16);
+                    }
+                    Route route = new Route(route_id, route_name, route_colour, route_text_colour);
+                    routes.put(route_name, route);
+                }
+                ++count;
+            }
+        }
+        catch (IOException ex)
+        {
+            Log.e("IOException", ex.getMessage());
+        }
+
+
+        return routes;
+    }
+
+
 }
--- app/src/main/java/com/pantone448c/ltccompanion/Routes.java
+++ app/src/main/java/com/pantone448c/ltccompanion/Routes.java
@@ -1,39 +1,33 @@
 package com.pantone448c.ltccompanion;
 
+import android.util.Log;
+
+import java.util.TreeMap;
+
 public final class Routes {
-    public static final Route ROUTE_1 = new Route(3962, 1, 0x70C2DA, 0x000000);
-    public static final Route ROUTE_2 = new Route( 3963, 2, 0x0080C0, 0x000000);
-    public static final Route ROUTE_3 = new Route(3964, 3, 0xC28585, 0x000000);
-    public static final Route ROUTE_4 = new Route(3965, 4, 0x7373B9, 0X000000);
-    public static final Route ROUTE_5 = new Route(3966, 5, 0x00FF00, 0x000000);
-    public static final Route ROUTE_6 = new Route(3967, 6, 0x008080, 0x000000);
-    public static final Route ROUTE_7 = new Route(3968, 7, 0x9D3CFF, 0x000000);
-    public static final Route ROUTE_9 = new Route(3969, 9, 0xB36666, 0x000000);
-    public static final Route ROUTE_10 = new Route(3970, 10, 0x96BEFA, 0x000000);
-    public static final Route ROUTE_102 = new Route(3971, 102, 0xC58AFF, 0x000000);
-    public static final Route ROUTE_104 = new Route(3972, 104, 0xC58AFF, 0x000000);
-    public static final Route ROUTE_106 = new Route(3973, 106, 0xC58AFF, 0x000000);
-    public static final Route ROUTE_12 = new Route(3975, 12, 0xCA95FF, 0x000000);
-    public static final Route ROUTE_13 = new Route(3976, 13, 0x927EC2, 0x000000);
-    public static final Route ROUTE_15 = new Route(3978, 15, 0x00D5D5, 0x000000);
-    public static final Route ROUTE_16 = new Route(3979, 16, 0x00AA55, 0x000000);
-    public static final Route ROUTE_17 = new Route(3980, 17, 0xFF2828, 0x000000);
-    public static final Route ROUTE_19 = new Route(3981, 19, 0x71D0FF, 0x000000);
-    public static final Route ROUTE_20 = new Route(3982, 20, 0x58ABAB, 0x000000);
-    public static final Route ROUTE_24 = new Route(3984, 24, 0xFF9BCD, 0x000000);
-    public static final Route ROUTE_25 = new Route(3985, 25, 0xDFDF00, 0x000000);
-    public static final Route ROUTE_27 = new Route(3987, 27, 0xFE9CA4, 0x000000);
-    public static final Route ROUTE_28 = new Route(3988, 28, 0x5555FF, 0x000000);
-    public static final Route ROUTE_30 = new Route(3990, 30, 0xCF298F, 0x000000);
-    public static final Route ROUTE_31 = new Route(3991, 31, 0x00B700, 0x000000);
-    public static final Route ROUTE_33 = new Route(3993, 33, 0xBB77FF, 0x000000);
-    public static final Route ROUTE_34 = new Route(3994, 34, 0x5959FF, 0x000000);
-    public static final Route ROUTE_35 = new Route(3995, 35, 0xDBC004, 0x000000);
-    public static final Route ROUTE_36 = new Route(3996, 36, 0xB43D61, 0x000000);
-    public static final Route ROUTE_37 = new Route(3997, 37, 0x0098E1, 0x000000);
-    public static final Route ROUTE_90 = new Route(4007, 90, 0xFF8A15, 0x000000);
-    public static final Route ROUTE_91 = new Route(4008, 91, 0xFF8000, 0x000000);
-    public static final Route ROUTE_92 = new Route(4009, 92, 0xFF8000, 0x000000);
-    public static final Route ROUTE_93 = new Route(4060, 93, 0xC0C0C0, 0x000000);
-    public static final Route ROUTE_94 = new Route(4061, 94, 0xFF0000, 0x000000);
+
+    public static void LoadRoutes(final TreeMap<Integer, Route> routes)
+    {
+        if (_routes == null)
+        {
+            _routes = new Routes(routes);
+        }
+        else
+        {
+            Log.i("AlreadyLoaded", "The static routes variable has already been initialized");
+        }
+    }
+
+    public static Routes getRoutes()
+    {
+        return _routes;
+    }
+
+    private Routes(final TreeMap<Integer, Route> routes)
+    {
+        this.routes = routes;
+    }
+    public final TreeMap<Integer, Route> routes;
+
+    private static Routes _routes;
 }
