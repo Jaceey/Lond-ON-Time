dba0705709dc755b6f63675a367789555f493a96
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2019-10-06 22:03:00 -0400

    Added some comments to Direction enum, getTripsByRouteDirection now returns a constant Array of Trips

--- app/src/main/java/com/pantone448c/ltccompanion/GTFSStaticData.java
+++ app/src/main/java/com/pantone448c/ltccompanion/GTFSStaticData.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.TreeMap;
 
 public class GTFSStaticData {
@@ -51,6 +53,138 @@ public class GTFSStaticData {
 
 
         return routes;
+    }
+
+    public static final Trip[] getTripsByRouteDirection(InputStream in, int route_id, Direction direction, int numTrips)
+    {
+        ArrayList<Trip> trips = new ArrayList<>();
+
+        Reader read = new InputStreamReader(in);
+        try
+        {
+            Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(read);
+            int count = 0;
+            int tripCount = 0;
+            for (CSVRecord record :records)
+            {
+                if (count > 1)
+                {
+                    if (Integer.parseInt(record.get(0)) == route_id)
+                    {
+                        if (numTrips > 0) //if numTrips is equal to 0 pull all the trips
+                        {
+                            if (tripCount < numTrips)
+                            {
+                                int service_id = Integer.parseInt(record.get(1));
+                                int trip_id = Integer.parseInt(record.get(2));
+                                String trip_headsign = record.get(3);
+                                Direction _direction;
+                                Wheelchair_Accessible wheelchair_accessible;
+                                Bikes_Allowed bikes_allowed;
+                                if (record.get(5) == "1")
+                                {
+                                    _direction = Direction.OTHERWAY;
+                                }
+                                else //if it's not otherway we'll just assume it's ONEWAY (it should only be a 0 or 1 but better to just default to ONEWAY
+                                {
+                                    _direction = Direction.ONEWAY;
+                                }
+
+                                if (record.get(8) == "1")
+                                {
+                                    wheelchair_accessible = Wheelchair_Accessible.WHEELCHAIRS;
+                                }
+                                else if (record.get(8) == "2")
+                                {
+                                    wheelchair_accessible = Wheelchair_Accessible.NOWHEELCHAIRS;
+                                }
+                                else
+                                {
+                                    wheelchair_accessible = Wheelchair_Accessible.NOINFO;
+                                }
+
+                                if (record.get(9) == "1")
+                                {
+                                    bikes_allowed = Bikes_Allowed.BIKES;
+                                }
+                                else if (record.get(9) == "2")
+                                {
+                                    bikes_allowed = Bikes_Allowed.NOBIKES;
+                                }
+                                else
+                                {
+                                    bikes_allowed = Bikes_Allowed.NOINFO;
+                                }
+                                trips.add(new Trip(route_id, service_id, trip_id, trip_headsign, _direction, wheelchair_accessible, bikes_allowed));
+                                ++tripCount;
+                            }
+                            else
+                            {
+                                break;
+                            }
+
+                        }
+                        else
+                        {
+                            int service_id = Integer.parseInt(record.get(1));
+                            int trip_id = Integer.parseInt(record.get(2));
+                            String trip_headsign = record.get(3);
+                            Direction _direction;
+                            Wheelchair_Accessible wheelchair_accessible;
+                            Bikes_Allowed bikes_allowed;
+                            if (record.get(5) == "1")
+                            {
+                                _direction = Direction.OTHERWAY;
+                            }
+                            else //if it's not otherway we'll just assume it's ONEWAY (it should only be a 0 or 1 but better to just default to ONEWAY
+                            {
+                                _direction = Direction.ONEWAY;
+                            }
+
+                            if (record.get(8) == "1")
+                            {
+                                wheelchair_accessible = Wheelchair_Accessible.WHEELCHAIRS;
+                            }
+                            else if (record.get(8) == "2")
+                            {
+                                wheelchair_accessible = Wheelchair_Accessible.NOWHEELCHAIRS;
+                            }
+                            else
+                            {
+                                wheelchair_accessible = Wheelchair_Accessible.NOINFO;
+                            }
+
+                            if (record.get(9) == "1")
+                            {
+                                bikes_allowed = Bikes_Allowed.BIKES;
+                            }
+                            else if (record.get(9) == "2")
+                            {
+                                bikes_allowed = Bikes_Allowed.NOBIKES;
+                            }
+                            else
+                            {
+                                bikes_allowed = Bikes_Allowed.NOINFO;
+                            }
+                            trips.add(new Trip(route_id, service_id, trip_id, trip_headsign, _direction, wheelchair_accessible, bikes_allowed));
+                        }
+
+                    }
+                }
+                ++count;
+            }
+        }
+        catch (IOException ex)
+        {
+            Log.e("IOException", ex.getMessage());
+        }
+        catch (IndexOutOfBoundsException ex)
+        {
+            Log.e("IndexOutofRange", ex.getMessage());
+        }
+        Trip[] tripsArray = new Trip[trips.size()];
+        tripsArray = trips.toArray(tripsArray); //convert ArrayList to straight array, don't need to return an entire arraylist since this shouldn't be edited.
+        return tripsArray;
     }
 
 
--- app/src/main/java/com/pantone448c/ltccompanion/Stops.java
+++
@@ -1,7 +0,0 @@
-package com.pantone448c.ltccompanion;
-
-import java.util.TreeMap;
-
-public class Stops {
-
-}
