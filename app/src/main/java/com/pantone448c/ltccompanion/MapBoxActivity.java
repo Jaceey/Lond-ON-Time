4705b9460e2e9f965e392e5edc658df90cfa8212
Matthew Wrobel <mattwrobel@hotmail.com>
2019-10-15 22:46:30 -0400

    Mapbox init configuration

---
+++ app/src/main/java/com/pantone448c/ltccompanion/MapBoxActivity.java
@@ -0,0 +1,14 @@
+package com.pantone448c.ltccompanion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_box);
+    }
+}
