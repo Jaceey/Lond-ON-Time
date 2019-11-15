f443ab7b6ae4badf1e4ee7503189cc0588337af8
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2019-11-14 23:29:46 -0500

    Currently trying to parse walking steps

--- app/src/main/java/com/pantone448c/ltccompanion/ui/directions/RouteBuilder.java
+++ app/src/main/java/com/pantone448c/ltccompanion/ui/directions/RouteBuilder.java
@@ -7,9 +7,13 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.RequestFuture;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 public class RouteBuilder
@@ -34,6 +38,11 @@ public class RouteBuilder
     {
         RequestFuture<JSONObject> future = RequestFuture.newFuture();
 
+        String arrivalTime;
+        String departureTime;
+        String distance;
+        String duration;
+
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(googleMapsDirectionAPI);
         stringBuilder.append("&origin=");
@@ -46,12 +55,42 @@ public class RouteBuilder
 
         try {
             JSONObject response = future.get(); // this will block
-            String arrivalTime = response.getJSONArray()
+            JSONObject route = response.getJSONArray("routes").getJSONObject(0);
+            JSONObject leg = route.getJSONArray("legs").getJSONObject(0);
+            JSONObject arrivalTimeObject = leg.getJSONObject("arrival_time");
+            arrivalTime = arrivalTimeObject.getString("text");
+            JSONObject departureTimeObject = leg.getJSONObject("departure_time");
+            departureTime = departureTimeObject.getString("text");
+            JSONObject distanceObject = leg.getJSONObject("distance");
+            distance = distanceObject.getString("text");
+            JSONObject durationObject = leg.getJSONObject("duration");
+            duration = durationObject.getString("text");
+            JSONArray steps = leg.getJSONArray("steps");
+            for (int i=0; i<steps.length(); ++i)
+            {
+                JSONObject step = steps.getJSONObject(i);
+                String stepDuration;
+                String stepDistance;
+                String instructions;
+                if (step.getString("travel_mode") == "WALKING")
+                {
+                    ArrayList<WalkingStep> subSteps = new ArrayList<>();
+                }
+                else if(step.getString("travel_mode") == "TRANSIT")
+                {
+
+                }
+            }
         } catch (InterruptedException e) {
             // exception handling
         } catch (ExecutionException e) {
             // exception handling
+        } catch (JSONException e)
+        {
+
         }
     }
+
+    private static
 
 }
